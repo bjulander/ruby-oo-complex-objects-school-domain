32c684ccd87b92e62fe153a8becf00f57bf7a73e
Blake Julander <13lakejulander@gmail.com>
2021-01-21 14:04:15 -0700

    commit tree d364930c407b551bf79ceff3ae0419b54d846967

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster 
+
+    def initialize(roster)
+        @roster = roster
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name 
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        empty = {}
+        @roster.each do |grade, name|
+            empty[grade] = name.sort
+        end
+        empty.sort.to_h
+    end
+end
